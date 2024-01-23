da76614230663c888b5cd7081ea3bc6e28adc048
Joy Liu <joyliu.q@gmail.com>
2024-01-23 04:14:51 -0500

    :new: Wooo

---
+++ README.md
@@ -0,0 +1,4 @@
+# Snippets
+Code I find cool
+
+Sandbox area for code I find cool that could be used for lectures or sandbox as the time comes.
---
+++ basics/src/bin/iterator.rs
@@ -0,0 +1,38 @@
+/**
+ * OWNERSHIP PREVIEW
+ * 
+ * Example of how iterators are lazily evaluated, so you could have an infinite iterator.
+ * 
+ * Concepts covered:
+ * - iterator trait
+ * - lazy evaluation
+ * - ownership: moving / borrowing / by_ref
+ * - closures :D
+ */
+fn infinite_iterator() {
+  let mut count = 0;
+  // Remove `mut` keyword
+  // Remove `move`
+  let mut iterator = std::iter::from_fn(move ||  {
+    count += 1;
+    Some(count)
+  });
+
+  // Guess; what happens if you call it as is?
+  // Remove `.by_ref()`. What do you think happens?
+  for i in iterator.by_ref().take(10) {
+    println!("{}", i);
+  }
+
+  for i in iterator.by_ref().take(10) {
+    println!("{}", i);
+  }
+
+  print!("Final value of `count` variable: {}", count);
+
+}
+
+
+fn main() {
+  infinite_iterator();
+}
